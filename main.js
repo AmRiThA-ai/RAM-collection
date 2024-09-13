6e1568d84b2c7bc0d661268ea744c2859d02bf27
AmRiThA-ai <amritha.balachandar1@education.nsw.gov.au>
2024-09-13 15:09:08 +1000

    javascript

---
+++ main.js
@@ -0,0 +1,10 @@
+function Escape() {
+    window.location.href = "https://exampleURL.com/";
+}
+function EscapeRoom() {
+    window.location.href = "https://exampleURL.com/";
+}
+
+function SuperSpy() {
+    window.location.href = "https://scratch.mit.edu/projects/996228818/";
+}
